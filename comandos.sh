672f66155fbacf709c9574c23746c93a79b9a10b
jac <me@jorge.red>
2020-06-02 00:25:20 +0200

    Revert "EA4HFO conecta con EA4GAX"
    This reverts commit 9ef12da5443fe122d245973e0484e0876cdc81af.

--- comandos.sh
+++ comandos.sh
@@ -1,4 +1,4 @@
-sudo kissattach /dev/pts/$1 EA4HFO
-sudo kissparms -p EA4HFO  -t 100 -l 30 -s 20 -r 80 -f n
-sudo ifconfig ax0 192.168.73.1/30
-sudo arp -H ax25 -s 192.168.73.2 EA4GAX
+sudo kissattach /dev/pts/$1 EA4GAX
+sudo kissparms -p EA4GAX  -t 100 -l 30 -s 20 -r 80 -f n
+sudo ifconfig ax0 192.168.73.2/30
+sudo arp -H ax25 -s 192.168.73.1 EA4HFO
