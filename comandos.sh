8462363f9dad777c46b0b935d5f0b78a1bf8f84d
jac <me@jorge.red>
2020-06-02 11:32:02 +0200

    EA4HFO - EA4GAX

--- comandos.sh
+++ comandos.sh
@@ -1,6 +1,6 @@
-sudo kissattach /dev/pts/$1 EA4GAX
-sudo kissparms -p EA4GAX  -t 100 -l 30 -s 20 -r 80 -f n
-sudo ifconfig ax0 192.168.73.3/24
-sudo arp -H ax25 -s 192.168.73.1 EA4HFO
+sudo kissattach /dev/pts/$1 EA4HFO
+sudo kissparms -p EA4HFO  -t 100 -l 30 -s 20 -r 80 -f n
+sudo ifconfig ax0 192.168.73.1/24
+#sudo arp -H ax25 -s 192.168.73.1 EA4HFO
 sudo arp -H ax25 -s 192.168.73.2 EA4GZU
 sudo arp -H ax25 -s 192.168.73.3 EA4GAX
